5df37eb706fa182314310506fa16e1e7631ba9ff
Ming <pc007ya@gmail.com>
2026-07-24 15:51:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='g1-chinese-v1';
+const CACHE='g1-chinese-v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./404.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
